ea2ad96fff8af23c4df55c8a2b19007123139540
moon-cup <47781583+moon-cup@users.noreply.github.com>
2019-09-11 15:48:49 +0800

    Create index.js

---
+++ src/api/index.js
@@ -0,0 +1,41 @@
+import axios from 'axios';
+
+let base="http://api.cat-shop.penkuoer.com";
+//拦截器
+
+// 添加请求拦截器
+axios.interceptors.request.use(function (config) {
+    // 在发送请求之前做些什么
+    if(localStorage.getItem('token')){
+    		config.headers.common['authorization'] = "Bearer "+localStorage.getItem('token');
+    }
+    return config;
+  }, function (error) {
+	// 对请求错误做些什么
+	console.log('请求错误')
+    return Promise.reject(error);
+  });
+
+// 添加响应拦截器
+axios.interceptors.response.use(function (response) {
+    // 对响应数据做点什么
+    return response;
+  }, function (error) {
+    // 对响应错误做点什么
+    return Promise.reject(error);
+  });
+
+
+  
+export const POST=(url,params)=>{
+	return axios.post(`${base}${url}`,params).then(res=>res)
+}
+export const GET=(url,params)=>{
+	return axios.get(`${base}${url}`,{params:params}).then(res=>res)
+}
+export const DELETE=(url,params)=>{
+	return axios.delete(`${base}${url}`,{params:params}).then(res=>res)
+}
+export const PUT=(url,params)=>{
+	return axios.put(`${base}${url}`,params).then(res=>res)
+}
