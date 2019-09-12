3aa6764c560111cd8c40abf66ad023b6d82c1c32
winner <465550479@qq.com>
2019-09-12 10:04:36 +0800

    Update Login.js

--- src/views/Login.js
+++ src/views/Login.js
@@ -1,11 +1,52 @@
-import React,{Component} from 'react';
+import React, { Component } from "react";
+import { Input } from "antd";
+import { Button } from "antd";
+import style from "../views/Login.module.css";
+import api from "../api/api_youdu";
 
-export default class Login extends Component{
-    render(){
-        return(
-            <div>
-                <h1>登陆</h1>
-            </div>
-        )
-    }
+export default class Login extends Component {
+  state = {
+    ipt1: "",
+    ipt2: ""
+  };
+  render() {
+    return (
+      <div>
+        <img
+          src="https://www.youdubook.com/static/index/Img/indexbanner.jpg"
+          className={style.img}
+        />
+        <span className={style.title}>有毒小说后台管理系统</span>
+        <div className={style.input1}>
+          用户名:
+          <Input placeholder="admin" className={style.ipt1} ref="ipt1" />
+        </div>
+        <div className={style.input2}>
+          密码:
+          <Input  placeholder="admin" className={style.ipt2} ref='ipt2'/>
+        </div>
+        <Button
+          type="primary"
+          block
+          className={style.button}
+          onClick={this.login.bind(this)}
+        >
+          登陆
+        </Button>
+      </div>
+    );
+  }
+  // 登陆
+  login() {
+    // console.log(this.refs.ipt1.state.value)
+    // console.log(this.refs.ipt2.state.value)
+    let params = {
+      userName: this.refs.ipt1.state.value,
+      password: this.refs.ipt2.state.value
+    };
+    api.adminLogin(params).then(data => {
+      // console.log(data);
+      alert('登陆成功!')
+    })
+  }
 }
