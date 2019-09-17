e51dba0074154b7092bd35492a9a72a562e6e8c5
moon-cup <47781583+moon-cup@users.noreply.github.com>
2019-09-17 08:11:30 +0800

    Create Qipao.js

---
+++ src/views/Qipao.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react'
+
+
+import PCard from './Card'
+
+import { Modal, Button } from 'antd';
+
+export default class Qipao extends React.Component {
+  state = { visible: false };
+
+  showModal = () => {
+    this.setState({
+      visible: true,
+    });
+  };
+
+  handleOk = e => {
+    console.log(e);
+    this.setState({
+      visible: false,
+    });
+  };
+
+  handleCancel = e => {
+    console.log(e);
+    this.setState({
+      visible: false,
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <Button type="primary" onClick={this.showModal}>
+          详情
+        </Button>
+        <Modal
+          title="Basic Modal"
+          visible={this.state.visible}
+          onOk={this.handleOk}
+          onCancel={this.handleCancel}
+        >
+          <PCard/>
+        </Modal>
+      </div>
+    );
+  }
+}
+
