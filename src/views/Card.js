ca223aff57f3071376d0bb47148f1171b4d8787d
moon-cup <47781583+moon-cup@users.noreply.github.com>
2019-09-18 20:42:42 +0800

    Update Card.js

--- src/views/Card.js
+++ src/views/Card.js
@@ -1,36 +1,55 @@
 import React, { Component } from 'react'
 import { Card, Icon, Avatar } from 'antd';
+import api from '../api/api_qin'
 
 const { Meta } = Card;
 
 export default class PCard extends Component {
+    state={
+        info:{}
+    }
     render() {
         return (
             <Card
-                style={{ width: "100%" }}
+                style={{ width: "70%",margin:"30px auto" }}
                 cover={
                     <img
-                        alt="example"
-                        src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
+                        alt="该用户没有上传图片"
+                        src={this.state.info.avatar}
                     />
                 }
                 actions={[
-                    <Icon type="setting" key="setting" />,
-                    <Icon type="edit" key="edit" />,
+                    <Icon type="rollback" key="setting" title='返回'
+                        onClick={this.back.bind(this)}/>,
+                    <Icon type="edit" key="edit" title="修改"
+                        onClick={this.edit.bind(this,this.state.info._id)}/>,
                     <Icon type="ellipsis" key="ellipsis" />,
                 ]}
             >
                 <Meta
-                    avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}
-                    title="Card title"
-                    description="This is the description"
+                    avatar={<Avatar src="https://www.youdubook.com/uploads/default_photo.jpg" />}
+                    title={this.state.info.userName}
+                    description={this.state.info.createdAt }
                 />
+                <p style={{margin:'10px',paddingLeft:"20px"}}><b>个性签名</b>：{this.state.info.nickName?this.state.info.nickName:"这个人很懒, 什么都没写..."}</p>
             </Card>
         )
     }
+    back(){
+        this.props.history.go('-1')
+    }
+    edit=(id)=>{
+        console.log(id);
+        this.props.history.push('/edit/'+id)
+    }
     componentDidMount(){
-        let userInfo = JSON.parse(localStorage.getItem('userInfo'))
-        console.log(userInfo)
+        console.log(this.props.match.params.id);
+        api.userinfo(this.props.match.params.id).then(res=>{
+            console.log(res);
+            this.setState({
+                info:res.data
+            })
+        })
     }
 }
 
