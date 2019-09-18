15ebaa38b4783c3161eff229c3e46dccbf6ca815
winner <465550479@qq.com>
2019-09-18 08:03:23 +0800

    Update Book.js

--- src/views/Book.js
+++ src/views/Book.js
@@ -68,7 +68,7 @@ class Book extends Component {
                   render: (id,key,index) => (
                       <span>
                           <Popconfirm
-                              title="警告:确认删除这本书吗?"
+                              title="提示:确认删除这本书吗?"
                               icon={<Icon type="question-circle-o" style={{ color: 'red' }} />}
                               onConfirm={this.Confirm.bind(this,id,key.key,index)}
                               okText="确认"
